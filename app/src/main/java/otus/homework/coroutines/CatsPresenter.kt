ba682ce968d10bcae0b04114d9d1c11b508da0ff
VirKato <dascat79@mail.ru>
2023-07-09 20:03:11 +0300

    обработка исключений

--- app/src/main/java/otus/homework/coroutines/CatsPresenter.kt
+++ app/src/main/java/otus/homework/coroutines/CatsPresenter.kt
@@ -1,21 +1,53 @@
 package otus.homework.coroutines
 
+import android.content.Context
+import android.widget.Toast
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.net.SocketTimeoutException
 
 class CatsPresenter(
     private val catsService: CatsService,
-    private val presenterScope: PresenterScope
+    private val presenterScope: PresenterScope,
+    private val context: Context
 ) {
 
     private var _catsView: ICatsView? = null
+    private val exceptionHandler =
+        CoroutineExceptionHandler { _, throwable -> handleException(throwable) }
 
     fun onInitComplete() {
-        presenterScope.launch { // ждём все подзадачи
-            val data = withContext(Dispatchers.IO) { catsService.getCatFact() } // нужен другой поток
-            _catsView?.populate(data)
+        presenterScope.launch(exceptionHandler) { // ждём все подзадачи
+            try {
+                val data = withContext(Dispatchers.IO) {
+//                    throw SocketTimeoutException()
+                    catsService.getCatFact()
+                } // нужен другой поток
+                _catsView?.populate(data)
+            } catch (e: SocketTimeoutException) {
+                // Обработка исключения SocketTimeoutException
+                handleSocketTimeoutException()
+            } catch (throwable: Throwable) {
+                // Обработка других исключений
+                handleOtherException(throwable)
+            }
         }
+    }
+
+    private fun handleException(throwable: Throwable) {
+        // Обработка общих исключений
+    }
+
+    private fun handleSocketTimeoutException() {
+        // Обработка исключения SocketTimeoutException
+        Toast.makeText(context, "Не удалось получить ответ от сервером", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun handleOtherException(throwable: Throwable) {
+        // Обработка других исключений
+        Toast.makeText(context, throwable.message, Toast.LENGTH_LONG).show()
     }
 
     fun attachView(catsView: ICatsView) {
--- app/src/main/java/otus/homework/coroutines/MainActivity.kt
+++ app/src/main/java/otus/homework/coroutines/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
         val view = layoutInflater.inflate(R.layout.activity_main, null) as CatsView
         setContentView(view)
 
-        catsPresenter = CatsPresenter(diContainer.service, presenterScope)
+        catsPresenter = CatsPresenter(diContainer.service, presenterScope, this)
         view.presenter = catsPresenter
         catsPresenter.attachView(view)
         catsPresenter.onInitComplete()
